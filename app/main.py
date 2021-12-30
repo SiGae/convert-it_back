615cbf1af2d79946429f2946d580704ec02a2ca4
SiGae <sigae@protonmail.com>
2021-12-30 21:48:52 +0900

    [update] allow cors all

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["127.0.0.1:8000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
